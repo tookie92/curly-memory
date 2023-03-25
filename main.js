2da16f5e248c142769a9d75c07622d53d5a86386
Joseph Ikinda <josephikinda@gmail.com>
2023-03-25 14:18:21 +0100

    sieht gut aus

--- main.js
+++ main.js
@@ -4,49 +4,96 @@ import "./style.css"
 import gsap from "gsap"
 import {OrbitControls} from "three/examples/jsm/controls/OrbitControls";
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import {RoomEnvironment} from 'three/addons/environments/RoomEnvironment.js';
+
+ //Secene declared
 
 
       const scene = new THREE.Scene();
-			const camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.1, 10000 );
+      scene.background = new THREE.Color(0xbfe3dd)
+      
+
+      //Camera
+			const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.1, 1000 );
+      camera.position.set(5,2,9);
+      camera.lookAt(0,2,0)
+      const mixers = [];
+      let mixer; 
+      const clock = new THREE.Clock();
+
+      const sizes={
+        width: window.innerWidth,
+        height: window.innerHeight,
+      }
       //light
-      const light = new THREE.HemisphereLight( 0xffffbb, 0x080820, 2 );
-       scene.add( light );
+      const hemiLight = new THREE.HemisphereLight( 0xffffff, 0x444444 );
+				hemiLight.position.set( 0, 20, 0 );
+				scene.add( hemiLight );
+
+				/*const dirLight = new THREE.DirectionalLight( 0xffffff );
+				dirLight.position.set( 0, 20, 10 );
+				scene.add( dirLight );*/
+     
 
       
 
 			const renderer = new THREE.WebGLRenderer();
 			renderer.setSize( window.innerWidth, window.innerHeight );
+      renderer.setPixelRatio(window.devicePixelRatio);
 			document.body.appendChild( renderer.domElement );
+
+      const pmremGenerator = new THREE.PMREMGenerator( renderer );
+      scene.environment = pmremGenerator.fromScene( new RoomEnvironment(), 0.04 ).texture;
+
 
 
        //controls
        const controls = new OrbitControls(camera, renderer.domElement);
-       camera.position.set(0,20,100);
-       controls.enableDamping = false;
-       controls.enableZoom = false;
-       controls.update();
+      controls.target.set(0,0.5,0);
+      controls.update();
+       controls.enableDamping = true;
+      controls.enableZoom = false;
+      controls.enablePan = false;
+      
 
 
        // Add 3d objects
 			const loader = new GLTFLoader();
-
-        loader.load( 'glt/chance/poly/iso.glb', function ( gltf ) {
-         
-          controls.update();
-         
+        loader.load( 'glt/chance/poly/hand.glb', function ( gltf ) {
+          const model = gltf.scene;
+          model.position.set(1,0,2);
+          model.scale.set(0.5, 0.5,0.5);
           scene.add( gltf.scene );  
+  
+         
+
+          mixer = new THREE.AnimationMixer(model);
+          const rotating = mixer.clipAction(gltf.animations[0]);
+          rotating.play();
+          rotating.loop = THREE.LoopOnce;
 
 
+          mixer.addEventListener('is finished', (e)=>{
+            window.alert('Animation is Finished');
+          })
         }, undefined, function ( error ) {
-
           console.error( error );
-
         } );
+			
+        window.onresize = function () {
 
-			camera.position.z = 5;
+          camera.aspect = window.innerWidth / window.innerHeight;
+          camera.updateProjectionMatrix();
+  
+          renderer.setSize( window.innerWidth, window.innerHeight );
+  
+        };
 
 			function animate() {
+        controls.update();
 				requestAnimationFrame( animate );
+        const delta = clock.getDelta();
+       mixer.update(delta);
 				renderer.render( scene, camera );
 			}
 
